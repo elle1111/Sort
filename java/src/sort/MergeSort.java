58bf183b8da92a848262ee5ad3dbce1a556e9cd8
elle1111 <15087003285@163.com>
2018-04-30 14:33:56 +0800

    上传归并排序代码

--- java/src/sort/MergeSort.java
+++ java/src/sort/MergeSort.java
@@ -5,28 +5,30 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args){
         //for(int i=0;i<100;i++){
-        int[] array=new int[]{3,1,2,4,6};
-        int[] arrayRandom=Main.createRandomArray(1000000);
-        int[] arrayRandomCopy=Arrays.copyOf(arrayRandom,1000000);
+        int[] array=new int[]{3,1,2,5,4};
+        int[] arrayRandom=Main.createRandomArray(100000);
+        int[] arrayRandomCopy=Arrays.copyOf(arrayRandom,100000);
         //PrintUtil.printArray(arrayRandom);
+        int[] arrayNew=new int[100000];
         long start1=System.currentTimeMillis();
         Arrays.sort(arrayRandomCopy);
         long end1=System.currentTimeMillis();
         System.out.println("系统排序时间为："+(end1-start1));
         MergeSort mergeSort=new MergeSort();
-        int flag=0;
+        //int flag=0;
         long start2=System.currentTimeMillis();
-        mergeSort.sort(arrayRandom,0,arrayRandom.length,flag);
+        mergeSort.sort(arrayRandom,0,arrayRandom.length,arrayNew);
+        //mergeSort.sort(array,0,array.length,arrayNew);
         long end2=System.currentTimeMillis();
         System.out.println("归并排序时间为："+(end2-start2));
         //PrintUtil.printArray(array);
         System.out.println(ArrayUtil.isTwoArrayEqual(arrayRandom,arrayRandomCopy));
     }
 //}
-    public void sort(int[] array,int left,int right,int flag){
+    public void sort(int[] array,int left,int right,int[] arrayNew){
         //System.out.println("最右边的元素下标为："+right);
         //System.out.println("最左边的元素下标为："+left);
-        flag++;
+
         //System.out.println("第次排序："+flag);
         //包含left,不包含right
         if((right-left)==1) return;
@@ -40,64 +42,96 @@ public class MergeSort {
        // System.out.println("Programmming is bug.");
 
 
-        sort(array,left,middle,flag);
-        int[] array1= Arrays.copyOfRange(array,left,middle);
+        sort(array,left,middle,arrayNew);
+        //int[] array1= Arrays.copyOfRange(array,left,middle);
         //System.out.println("左边");
-        //PrintUtil.printArray(array1);
+        //PrintUtil.printArray(array);
         //System.out.println("Programmming is bug.");
 
-        sort(array,middle,right,flag);
-        int[] array2=Arrays.copyOfRange(array,middle,right);
+        sort(array,middle,right,arrayNew);
+        //int[] array2=Arrays.copyOfRange(array,middle,right);
         //System.out.println("右边");
-        //PrintUtil.printArray(array2);
-        int[] array3;
-        array3= merge(array1,array2);
+        //PrintUtil.printArray(array);
+        //int[] array3;
+        //array3= merge(array1,array2);
+        merge(array,left,middle,right,arrayNew);
         //System.out.println("合并");
+
         int q=left;
-        //PrintUtil.printArray(array3);
-        for(int i=0;i<array3.length;i++){
-           array[q]=array3[i];
+        //PrintUtil.printArray(array);
+        //
+        //
+        for(int i=left;i<right;i++){
+           array[q]=arrayNew[i];
            q++;
         }
+        //PrintUtil.printArray(arrayNew);
         //PrintUtil.printArray(array);
     }
-    public int[] merge(int[] arrayLeft,int[] arrayRight){
+    public int[] merge(int[] array,int left,int middle,int right,int[] arrayNew){
         //array1,array2是待传入排序的数组，array3是排序后的数组
-        int i=arrayLeft.length;
-        int j=arrayRight.length;
-        int[] array=new int[i+j];
-            for(int q=0;q<arrayLeft.length+arrayRight.length;q++) {
+        int i=0;
+        int j=0;
+        for(int q=0;q<right-left;q++){
+            if((i<middle-left)&&(j<right-middle))
+            {
+                if(array[left+i]>array[middle+j]){
+                    arrayNew[left+q]=array[middle+j];
+                    j++;
+                }
+                else{
+                    arrayNew[left+q]=array[left+i];
+                    i++;
+                }
+            }
+            else if(i==middle-left){
+                arrayNew[left+q]=array[middle+j];
+                j++;
+            }
+            else if(j==right-middle){
+                arrayNew[left+q]=array[left+i];
+                i++;
+            }
+
+        }
+
+/*
+        int i=middle-left;
+        int j=right-middle;
+        //int[] array=new int[i+j];
+            for(int q=0;q<right-left;q++) {
                 //如果两个数组长度均大于0
                 if(i>0&&j>0){
                     //如果右边数组的元素小于左边数组的元素
-                    if(arrayLeft[arrayLeft.length-i]>arrayRight[arrayRight.length-j]){
+                    if(array[middle-left+1-i]>array[right-left+1-j]){
                         //将右边数组的元素赋值给array
-                        array[q]=arrayRight[arrayRight.length-j];
+                        arrayNew[right-left-1+q]=array[right+1-left-j];
                         j--;
                         //System.out.println(j);
                         //比较的元素向后移动一位
                     }
                     else{
                         //否则，将左边数组的元素赋值给array
-                        array[q]=arrayLeft[arrayLeft.length-i];
+                        arrayNew[right-left-1+q]=array[middle+1-left-i];
                         i--;
                         //System.out.println(i);
                         //比较的元素向后移动一位
                     }
                 }
             else if(i==0){
-                    array[q]=arrayRight[arrayRight.length-j];
+                    arrayNew[right-left-1+q]=array[right-left+1-j];
                     j--;
                     }
                     else{
-                    array[q]=arrayLeft[arrayLeft.length-i];
+                    arrayNew[right-left-1+q]=array[middle+1-left-i];
                     i--;
                 }
 
             }
-            //PrintUtil.printArray(arrayLeft);
+            */
+            //PrintUtil.printArray(arrayNew);
             //PrintUtil.printArray(arrayRight);
             //PrintUtil.printArray(array);
-            return array;
+            return arrayNew;
     }
 }
